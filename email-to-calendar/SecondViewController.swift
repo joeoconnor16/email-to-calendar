3b29d65e333c2490cbf73d54ebe6e4e5d956a3df
Joseph OConnor <josephoconnor@Josephs-Mac-mini.local>
2018-04-22 10:10:21 -0400

    Initial Commit

---
+++ email-to-calendar/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  email-to-calendar
+//
+//  Created by Joseph OConnor on 4/22/18.
+//  Copyright © 2018 Joseph OConnor. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ email-to-calendarTests/email_to_calendarTests.swift
@@ -0,0 +1,36 @@
+//
+//  email_to_calendarTests.swift
+//  email-to-calendarTests
+//
+//  Created by Joseph OConnor on 4/22/18.
+//  Copyright © 2018 Joseph OConnor. All rights reserved.
+//
+
+import XCTest
+@testable import email_to_calendar
+
+class email_to_calendarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
